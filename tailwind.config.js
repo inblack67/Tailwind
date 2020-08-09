b81e05fbb0a149d8c419ac26efb3e02da0a5bccd
Aman <devproddeploy@gmail.com>
2020-08-09 19:29:16 +0530

    card

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   purge: [],
   theme: {
+    extend: {
+      spacing: {
+        '1/2': '50%',
+        '1/3': '33.33%',
+        '2/3': '66.66%',
+      }
+    },
     container: {
       center: true,
       padding: '2rem'
